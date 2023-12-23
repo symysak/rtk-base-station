9386ffb618585f2d43a0c653c5cf2d09b2436106
Yama <103905928+symysak@users.noreply.github.com>
2023-12-23 10:51:10 +0900

    Update sync.py to show debug status messages

--- sync-docker-compose/sync.py
+++ sync-docker-compose/sync.py
@@ -5,7 +5,8 @@ import os
 debug =  os.environ.get("DEBUG", False)
 
 if os.environ.get("BRANCH") == None:
-    print("BRANCH is not set. Use default branch: main")
+    if debug:
+        print("BRANCH is not set. Use default branch: main")
     branch = "main"
 else:
     branch = os.environ.get("BRANCH")
@@ -14,15 +15,19 @@ url = "https://raw.githubusercontent.com/symysak/rtk-base-station/" + branch + "
 
 req = urllib.request.Request(url)
 with urllib.request.urlopen(req) as res:
-   new_docker_compose = res.read().decode("utf-8")
+    new_docker_compose = res.read().decode("utf-8")
 
 with open("docker-compose.yml", "r") as f:
-   old_docker_compose = f.read()
+    old_docker_compose = f.read()
+    f.close()
 
 if new_docker_compose != old_docker_compose:
     with open("docker-compose.yml", "w") as f:
         f.write(new_docker_compose)
         f.close()
     if debug:
-        print("docker-compose.yml updated")
+        print("docker-compose.yml was updated")
     subprocess.run("docker compose up -d", shell=True)
+else:
+    if debug:
+        print("docker-compose.yml is not changed")
