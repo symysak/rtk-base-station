4a18f2a3e0b33074dcf3c9d803ab0bb4a2e5fe31
Yama <yoshiaki.suyama@suyama.ne.jp>
2023-12-25 00:22:11 +0900

    Add script to remove containers

---
+++ rm_containers.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+systemctl --user stop container-str2str.service
+systemctl --user disable container-str2str.service
+systemctl --user stop container-ntrip-caster.service
+systemctl --user disable container-ntrip-caster.service
+podman rm -f str2str
+podman rm -f ntrip-caster
