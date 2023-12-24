e6a567110c8a6bd68b0916613a7e7153c1277b70
Yama <yoshiaki.suyama@suyama.ne.jp>
2023-12-24 22:19:53 +0900

    Fix podman run

--- run_containers.sh
+++ run_containers.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 podman run -d --name str2str --platform linux/arm64 -l io.containers.autoupdate=registry -l PODMAN_SYSTEMD_UNIT=container-str2str.service --group-add keep-groups --device /dev/ttyACM0:/dev/ttyACM0 --net=host ghcr.io/symysak/str2str:latest
-
+systemctl --user enable container-str2str.service
 podman run -d --name ntrip-caster --platform linux/arm64 -l io.containers.autoupdate=registry -l PODMAN_SYSTEMD_UNIT=container-ntrip-caster.service --net=host --env-file=.env ghcr.io/symysak/ntrip-caster:latest
+systemctl --user enable container-ntrip-caster.service
