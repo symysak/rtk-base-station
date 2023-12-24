229e6b920588969054710f0f8e5b60eb36cc0212
Yama <yoshiaki.suyama@suyama.ne.jp>
2023-12-24 23:08:59 +0900

    Update entrypoint.sh to use 'exec' for running str2str

--- ntrip-caster/entrypoint.sh
+++ ntrip-caster/entrypoint.sh
@@ -2,4 +2,4 @@
 
 out="${out_stream}/${mountpoint}:${mountpoint};rtcm3.3;${ntrip_msg};${carrer};GPS+GLO+GAL+BDS+QZS;Raspberry Pi;${country};${short_lat};${short_lon};0;0;github.com/symysak/rtk-base-station;NONE;${auth};N;;#rtcm3"
 
-/app/str2str -in tcpcli://localhost:2102#rtcm3 -out "${out}" -p ${lat} ${lon} ${hgt} -msg ${ntrip_msg}
+exec /app/str2str -in tcpcli://localhost:2102#rtcm3 -out "${out}" -p ${lat} ${lon} ${hgt} -msg ${ntrip_msg}
