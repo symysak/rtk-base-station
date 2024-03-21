bffcb799ebbae28cd1015214175faa55ef8c8dea
symysak <yoshiaki.suyama@suyama.ne.jp>
2024-03-22 00:14:23 +0900

    [mgmt-dashboard] Add a EasySettings page

--- mgmt-dashboard-frontend/src/pages/EasySettings.jsx
+++ mgmt-dashboard-frontend/src/pages/EasySettings.jsx
@@ -1,11 +1,95 @@
 import Layout from '../layout';
-import { PageSection } from '@patternfly/react-core';
+import {
+    Button,
+    Card,
+    CardBody,
+    Stack,
+    StackItem,
+    FormGroup,
+    FormHelperText,
+    HelperText,
+    HelperTextItem,
+    Modal,
+    Spinner,
+
+    PageSection,
+    TextInput
+} from '@patternfly/react-core';
+import { useState } from 'react';
+import cockpit from '../cockpit/pkg/lib/cockpit';
 
 function EasySettings() {
+    const [isGetCommnadFinished, setIsGetCommnadFinished] = useState(false);
+
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const cliPath = "~/rtk-base-station/mgmt-cli/mgmt-cli";
+    const configPath = "~/rtk-base-station/config/";
+    const sendGetCommand = async (key) => {
+        let res = undefined;
+        await cockpit.script(`${cliPath} -c ${configPath} get ${key}`)
+            .then(data => {
+                res = data.slice(0, -1);
+                setIsGetCommnadFinished(true);
+                console.log(`[GET] ${key}: "${res}"`);
+            }).catch(exception => {
+                setIsGetCommnadFinished(false);
+                console.error(`Failed to get ${key}`);
+                console.error(exception);
+            });
+        return res;
+    };
+
+    useState(() => {
+        (async () => {
+            setUsername(await sendGetCommand("Ntripcaster.Username"));
+            setPassword(await sendGetCommand("Ntripcaster.Password"));
+        })()
+    }, [])
+
     return (
         <Layout>
             <PageSection>
-                
+                <Modal
+                    isOpen={!isGetCommnadFinished}
+                    showClose={false}
+                    width="130px"
+                >
+                    <Button variant="tertiary"><Spinner /></Button>
+                </Modal>
+                <Stack>
+                    <StackItem>
+                        <Card>
+                            <CardBody>
+                                <FormGroup label="接続用ユーザ名">
+                                    <TextInput
+                                        value={username}
+                                        onChange={(_, value) => setUsername(value)}
+                                    />
+                                    <FormHelperText>
+                                        <HelperText>
+                                            <HelperTextItem>空欄の場合、設定なしです。</HelperTextItem>
+                                        </HelperText>
+                                    </FormHelperText>
+                                </FormGroup>
+                                <FormGroup label="接続用パスワード">
+                                    <TextInput
+                                        value={password}
+                                        onChange={(_, value) => setUsername(value)}
+                                    />
+                                    <FormHelperText>
+                                        <HelperText>
+                                            <HelperTextItem>空欄の場合、設定なしです。</HelperTextItem>
+                                        </HelperText>
+                                    </FormHelperText>
+                                </FormGroup>
+                            </CardBody>
+                        </Card>
+                    </StackItem>
+                </Stack>
+
             </PageSection>
         </Layout>
     );
