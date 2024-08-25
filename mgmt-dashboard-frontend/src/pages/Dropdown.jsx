f8e8da32c64598b2be1ee1616907492fee80d704
lion <teles_tech@icloud.com>
2024-08-25 23:33:23 +0900

    Commit

--- mgmt-dashboard-frontend/src/pages/Dropdown.jsx
+++ mgmt-dashboard-frontend/src/pages/Dropdown.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Alert, PageSection } from '@patternfly/react-core';
-import {Select, SelectOption, SelectList, MenuToggle, MenuToggleElement} from '@patternfly/react-core';
+import {Select, SelectOption, SelectList, MenuToggle} from '@patternfly/react-core';
 import BellIcon from '@patternfly/react-icons/dist/esm/icons/bell-icon';
 import Layout from '../layout';
 const Status = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
-  const [selected, setSelected] = React.useState('Select a value');
+  const [isOpen1, setIsOpen1] = React.useState(false);
+  const [selected1, setSelected1] = React.useState('Select a value');
   const onToggleClick = () => {
-    setIsOpen(!isOpen);
+    setIsOpen1(!isOpen1);
   };
   const onSelect = (_event, value) => {
     console.log('selected', value);
-    setSelected(value);
-    setIsOpen(false);
+    setSelected1(value);
+    setIsOpen1(false);
   };
-  const toggle = toggleRef => <MenuToggle ref={toggleRef} onClick={onToggleClick} isExpanded={isOpen} style={{
+  const toggle1 = toggleRef => <MenuToggle ref={toggleRef} onClick={onToggleClick} isExpanded={isOpen1} style={{
     width: '200px'
   }}>
-      {selected}
+      {selected1}
     </MenuToggle>;
   return (
     <Layout>
         <PageSection>
-        <Select id="option-variations-select" isOpen={isOpen} selected={selected} onSelect={onSelect} onOpenChange={isOpen => setIsOpen(isOpen)} toggle={toggle} shouldFocusToggleOnSelect>
+        <Select id="option-variations-select" isOpen={isOpen1} selected={selected1} onSelect={onSelect} onOpenChange={isOpen1 => setIsOpen1(isOpen1)} toggle={toggle1} shouldFocusToggleOnSelect>
         <SelectList>
         <SelectOption value="Basic option">Basic option</SelectOption>
         <SelectOption value="Option with description" description="This is a description">
